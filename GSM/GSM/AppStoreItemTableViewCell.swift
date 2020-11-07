0f624d27448a2f0cafc7ebe8683661c35f1ac69d
jo030225 <whwngur123@naver.com>
2020-11-08 01:39:23 +0900

    Jercy 숙제 완료
    imageView와 button에 cornerRadius도 추가

--- GSM/GSM/AppStoreItemTableViewCell.swift
+++ GSM/GSM/AppStoreItemTableViewCell.swift
@@ -8,45 +8,68 @@
 import UIKit
 
 final class AppStoreItemTableViewCell: UITableViewCell {
-
     var isUpdateConstraint: Bool = false
-    
     var thumbnailImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.backgroundColor = .red
+        imageView.layer.cornerRadius = 8
         return imageView
     }()
     var titleLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "재르시 키보드"
         return label
     }()
     var descriptionLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "jeasungLEE"
         return label
     }()
     var openButton: UIButton = {
         let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("보기", for: .normal)
+        button.backgroundColor = .blue
+        button.layer.cornerRadius = 17
         return button
     }()
     
     override func updateConstraints() {
         if !isUpdateConstraint {
             isUpdateConstraint = true
+            
+            addSubview(thumbnailImageView)
+            addSubview(titleLabel)
+            addSubview(descriptionLabel)
+            addSubview(openButton)
+            
+            thumbnailImageView.topAnchor.constraint(equalTo: topAnchor, constant: 16).isActive = true
+            thumbnailImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+            thumbnailImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -15).isActive = true
+            thumbnailImageView.widthAnchor.constraint(equalToConstant: 60).isActive = true
+            thumbnailImageView.heightAnchor.constraint(equalToConstant: 60).isActive = true
+            
+            
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 22).isActive = true
+            titleLabel.leadingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor, constant: 10).isActive = true
+            titleLabel.trailingAnchor.constraint(equalTo: openButton.leadingAnchor, constant: 15).isActive = true
+            
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 0).isActive = true
+            descriptionLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor, constant: 0).isActive = true
+            descriptionLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: 0).isActive = true
+            
+            openButton.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 0).isActive = true
+            openButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15).isActive = true
+            openButton.widthAnchor.constraint(equalToConstant: 66).isActive = true
         }
-        
-        addSubview(thumbnailImageView)
-        
-        
-        thumbnailImageView.topAnchor.constraint(equalTo: topAnchor, constant: 16).isActive = true
-        thumbnailImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
-        thumbnailImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -15).isActive = true
-        thumbnailImageView.widthAnchor.constraint(equalToConstant: 60).isActive = true
-        thumbnailImageView.heightAnchor.constraint(equalToConstant: 60).isActive = true
-        
         
         super.updateConstraints()
     }
+}
+
     
     
     
@@ -83,4 +106,4 @@ final class AppStoreItemTableViewCell: UITableViewCell {
 //        fatalError("init(coder:) has not been implemented")
 //    }
     
-}
+
--- GSM/GSM/SearchViewController.swift
+++ GSM/GSM/SearchViewController.swift
@@ -10,26 +10,22 @@ import UIKit
 class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        5
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = AppStoreItemTableViewCell()
         
+        cell.updateConstraintsIfNeeded()
+        
         return cell
     }
-    
 
     @IBOutlet weak var tableView: UITableView! {
         didSet {
             tableView.delegate = self
             tableView.dataSource = self
+
         }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
     }
 }
